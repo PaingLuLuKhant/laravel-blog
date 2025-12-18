607f84d9700ed74ed008d8ad9157e358b44e43c8
PaingLuLuKhant <2016-miit-cse-029@miit.edu.mm>
2025-12-18 09:48:41 +0630

    added Post,User and Like Models and factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,14 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Article;
+use App\Models\Post;
+use App\Models\Comment;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Like;
+
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +20,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Article::factory(10)->create();
+
+        Post::factory(10)->create(); 
+        Comment::factory(15)->create();
+        Like::factory(30)->create();
     }
 }
