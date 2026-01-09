20e9ec18fe0abc7a57b75d2f437502d33d23de40
PaingLuLuKhant <2016-miit-cse-029@miit.edu.mm>
2026-01-09 08:23:48 +0630

    breeze

--- routes/web.php
+++ routes/web.php
@@ -1,58 +1,20 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\LikeController;
-
-
-
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/articles', [ArticleController::class, 'index']); // redirect to controller
-Route::get('/posts', [PostController::class, 'index']); // redirect to controller
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-
-Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
-
-Route::get('/test-relation', [UserController::class, 'index']);
-Route::get('/profile', [ProfileController::class, 'index']);
-Route::get('/post-list', [UserController::class, 'postList']);
-Route::get('/post-by-user   ', [PostController::class, 'postedUser']);
-Route::get('/user/likes', [LikeController::class, 'showLikedPosts']);
-Route::get('/post/likers', [LikeController::class, 'showPostLikers']);
-Route::get('/user/{id}/latest-comment', [UserController::class, 'showLatestComment']);
-Route::get('/user/{id}/comments', [UserController::class, 'showUserComments']);
-
-Route::get('/category/{id}', [CategoryController::class, 'showPostsByCategory']);
-Route::get('/post/{id}/category', [CategoryController::class, 'showCategoryOfPost']);
-
-// Route::get('/articles', function () {
-//  return 'Article List';
-// });
-
-// Route::get('/articles/detail', function () {
-//  return 'Article Detail Page';
-// })->name('article.detail');
-
-// // dynamic route
-// Route::get('/articles/detail/{id}', function ( $id ) {
-//  return "Article Detail - $id";
-// });
-// // redirect
-// Route::get('/articles/more', function() {
-//  return redirect('/articles/detail');
-// });
-
-
-
-
-
+require __DIR__.'/auth.php';
